53fcd27f032d5455fce19e0936440f849427039a
jesus-333 <albi25.zanca@gmail.com>
2021-08-19 23:53:09 +0200

    Add dice app layout and relative scene change

--- app/src/main/java/com/example/appsofgoogletutorial/MainActivity.kt
+++ app/src/main/java/com/example/appsofgoogletutorial/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.appsofgoogletutorial
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonDiceApp: Button = findViewById(R.id.buttonDiceApp)
+        buttonDiceApp.setOnClickListener{changeScene(1)}
+    }
+
+    private fun changeScene(select_app: Int){
+        when(select_app){
+            1 -> startActivity(Intent(this, DiceAppActivity::class.java))
+        }
+
     }
 }
