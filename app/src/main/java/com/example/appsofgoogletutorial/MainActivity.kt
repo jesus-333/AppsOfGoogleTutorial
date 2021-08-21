a5093d7729d694c1fdfe00f4f47eede55eec8c52
jesus-333 <albi25.zanca@gmail.com>
2021-08-21 17:16:18 +0200

    Add tip calculator app layout

--- app/src/main/java/com/example/appsofgoogletutorial/MainActivity.kt
+++ app/src/main/java/com/example/appsofgoogletutorial/MainActivity.kt
@@ -11,15 +11,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val buttonDiceApp: Button = findViewById(R.id.buttonDiceApp)
-        buttonDiceApp.setOnClickListener{changeScene(1)}
+        buttonDiceApp.setOnClickListener { changeScene(1) }
 
         val buttonTipCalculatorApp: Button = findViewById(R.id.buttonTipCalculatorApp)
-        buttonTipCalculatorApp.setOnClickListener{changeScene(2)}
+        buttonTipCalculatorApp.setOnClickListener { changeScene(2) }
     }
 
-    private fun changeScene(select_app: Int){
+    private fun changeScene(select_app: Int) {
         //Toast.makeText(this, "${select_app}", Toast.LENGTH_SHORT).show()
-        when(select_app){
+        when (select_app) {
             1 -> startActivity(Intent(this, DiceAppActivity::class.java))
             2 -> startActivity(Intent(this, TipCalculatorActivity::class.java))
         }
